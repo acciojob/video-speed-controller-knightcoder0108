fa519a7d7a0d1ca09294dd29666cdf05097c754d
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-07-02 22:52:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.getElementById('video');
+const playerButton = document.querySelector('.player__button');
+const progress = document.querySelector('.progress');
+const progressBarFilled = document.querySelector('.progress__filled');
+const volumeInput = document.querySelector('.volume');
+const playbackSpeedInput = document.querySelector('.playbackSpeed');
+const rewindButton = document.querySelector('.rewind');
+const forwardButton = document.querySelector('.forward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+let isPlaying = false;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+playerButton.addEventListener('click', togglePlayPause);
+progress.addEventListener('input', updateProgressBar);
+volumeInput.addEventListener('input', updateVolume);
+playbackSpeedInput.addEventListener('input', updatePlaybackSpeed);
+rewindButton.addEventListener('click', rewindVideo);
+forwardButton.addEventListener('click', forwardVideo);
+
+function togglePlayPause() {
+  if (isPlaying) {
+    video.pause();
+    playerButton.dataset.icon = '►';
+    isPlaying = false;
+  } else {
+    video.play();
+    playerButton.dataset.icon = '❚ ❚';
+    isPlaying = true;
+  }
+}
+
+function updateProgressBar() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBarFilled.style.flexBasis = `${progressPercentage}%`;
+}
+
+function updateVolume() {
+  video.volume = volumeInput.value / 100;
+}
+
+function updatePlaybackSpeed() {
+  video.playbackRate = playbackSpeedInput.value;
+}
+
+function rewindVideo() {
+  video.currentTime -= 10;
+}
+
+function forwardVideo() {
+  video.currentTime += 25;
+}
